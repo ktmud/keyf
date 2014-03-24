9f12d55eb87d05a9598b7c2f2b5a18c679faeb06
Jesse Yang <jyyjcc@gmail.com>
2014-03-24 19:30:03 +0800

    use hasOwnProperty to get value

--- index.js
+++ index.js
@@ -22,19 +22,21 @@ function getFormatter(key, predefined) {
 
   predefined = predefined || {}
 
-  return function keyf() {
+  return function keyf(args) {
     var ks, v, k
     var self = this
     return key.replace(KEY_PATTERN, function(m, format, p1) {
       ks = p1.split('.')
       v = null
       k = ks.shift()
-      if (predefined.hasOwnProperty(k)) {
-        v = predefined[k]
+      if (args.hasOwnProperty(k)) {
+        v = args[k] // get from arguments
+      } else if (predefined.hasOwnProperty(k)) {
+        v = predefined[k] // get from pre-defined
       } else if (k === 'this') {
         v = self
       } else {
-        v = isdigit(k) ? args[k] : self[k]
+        v = self[k] // get value from context
       }
       // dive into object
       while (v && ks.length && ks[0] in v) {
@@ -51,10 +53,6 @@ function getFormatter(key, predefined) {
       return v || m
     })
   }
-}
-
-function isdigit(s) {
-  return !isNaN(Number(s))
 }
 
 module.exports = getFormatter
